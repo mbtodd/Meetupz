8e580bddc589be97235286655bef478d2c5bfd65
Michael Todd <michaelbtodd@yahoo.com>
2019-03-22 17:50:40 -0400

    First commit

---
+++ client/precache-manifest.9bc29b74d5b722eebdf33357241abf28.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "783818447e4364aec9ac",
+    "url": "/static/js/main.2cfa8530.chunk.js"
+  },
+  {
+    "revision": "e910bbdf62c51e54264e",
+    "url": "/static/js/2.a44378ae.chunk.js"
+  },
+  {
+    "revision": "783818447e4364aec9ac",
+    "url": "/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "df71bcb7bf4c58929300652017b82855",
+    "url": "/index.html"
+  }
+];
